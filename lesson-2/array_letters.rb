c84db3185df60973aa1947453cd21a850fc6f7d0
Valentyn Gevrasov <ValAntG@gmail.com>
2019-06-26 17:48:04 +0300

    Lesson2# Fix2 by PR

--- lesson-2/array_letters.rb
+++ lesson-2/array_letters.rb
@@ -1,6 +1,5 @@
 letter = 'а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я'
 letter_array = letter.split(' ')
-vowels_array = 'а е ё и о у э ю я'.split(' ')
 vowels_hash = {}
-letter_array.each_with_index { |lett, nomer| vowels_hash[lett] = nomer + 1 if vowels_array.include?(lett) }
+letter_array.each_with_index { |lett, nomer| vowels_hash[lett] = nomer + 1 if 'аеёиоуэюя'.include?(lett) }
 puts vowels_hash
